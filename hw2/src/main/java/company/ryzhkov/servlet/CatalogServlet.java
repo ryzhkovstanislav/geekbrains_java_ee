5d99dc3c7e022b2bd7455270cd61acfe1e046f86
ryzhkovstanislav <stanislav.rv@gmail.com>
2018-10-08 19:50:37 +0700

    Домашнее задание к уроку 2

---
+++ hw2/src/main/java/company/ryzhkov/servlet/CatalogServlet.java
@@ -0,0 +1,18 @@
+package company.ryzhkov.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/catalog")
+public class CatalogServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/catalog.html").forward(req, resp);
+    }
+}
